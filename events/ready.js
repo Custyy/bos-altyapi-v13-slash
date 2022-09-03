d88d9d855489a01163ee937f6abdf1f83b09d0eb
Custy <69308633+custyTR@users.noreply.github.com>
2022-09-04 00:38:03 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js')
 
 module.exports = (client) => {
-
-  client.user.setActivity(`${Discord.version} Slash Template`, { type: 'PLAYING', url: 'https://twitch.tv/ryper_1' })
+  client.user.setActivity(`🎁 (change me from events"/ready.js")`, { type: 'PLAYING', url: 'https://twitch.tv/ryper_1' })
   console.log(`${client.user.username} Aktif! (${client.guilds.cache.size} Sunucu - ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} Kullanıcı)`)
 }
